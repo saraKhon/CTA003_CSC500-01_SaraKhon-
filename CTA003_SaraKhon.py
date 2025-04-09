23781483a97bd4c79f3ab9edf8a086b8e81ebc4e
saraKhon <saravenus.khon@csuglobal.edu>
2025-04-09 00:13:48 -0600

    CTA03 Complete

--- CTA003_SaraKhon.py
+++ CTA003_SaraKhon.py
@@ -1,8 +1,3 @@
-from cherrypy.tutorial.tut02_expose_methods import HelloWorld
-
-print(HelloWorld)
-
-
 """
 Student: Saravenus Khon, Date: 04/08/2025, CTA03 - Creating Python Programs
 """
@@ -29,6 +24,27 @@ def total_meals_purchased():
 #run the functon
 total_meals_purchased()
 
-
+print("\n ----------------------------------------------------------------------")
 
 #Part II:
+
+def AlarmTime_Calculation():
+    # ask the user for the current time in 24hr format
+    current_time = int(input("Enter the current Military Time (0 hr to 23 hr): "))
+
+    # Ask the user to input the amount of hours to wait for the alam
+    hours_to_wait = int(input("Now, enter how many hours you want to wait: "))
+
+    #process the calculations for the alarm to ring /24hrs
+    alarm = (current_time+hours_to_wait) % 24
+
+    #print out the results
+    print(f"Your alarm will go off at {alarm}:00 on a 24 hr clock.")
+
+
+#run the function
+AlarmTime_Calculation()
+
+
+
+
